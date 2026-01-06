0160f432d96cf92a49568c93820016bdd1cdbaa2
mothibimalebana <mothibi.mm12@gmail.com>
2026-01-06 20:38:54 +0200

    1.7 Web server

--- hello.go
+++ hello.go
@@ -1,49 +1,31 @@
-// Lissajous generates GIF animations of random Lissajous figures.
+// Fetch prints the content found at a URL.
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/gif"
+	"fmt"
 	"io"
-	"math"
-	"math/rand"
+	"net/http"
 	"os"
-)
-
-var palette = []color.Color{color.White, color.Black}
-
-const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
+	"strings"
 )
 
 func main() {
-	lissajous(os.Stdout)
-}
-func lissajous(out io.Writer) {
-	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-	)
-	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 // phase difference
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
 		}
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v", err)
+		}
+		b, err := io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: read: %v", err)
+		}
+		fmt.Printf("<------------Body Section-------->\n")
+		fmt.Printf("%v\n", b)
+		fmt.Printf("<------------Header Section-------->\n")
+		fmt.Printf("%v\n", resp.Header)
 	}
-	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
