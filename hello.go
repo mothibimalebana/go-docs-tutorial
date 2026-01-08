58e7e5929656ecbc0d2d75fda1e8ae2fc607287b
mothibimalebana <mothibi.mm12@gmail.com>
2026-01-08 09:27:29 +0200

    flag package

--- hello.go
+++ hello.go
@@ -2,30 +2,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
 	"strings"
 )
 
-func main() {
-	for _, url := range os.Args[1:] {
-		if !strings.HasPrefix(url, "http://") {
-			url = "http://" + url
-		}
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "seperator")
 
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v", err)
-		}
-		b, err := io.Copy(os.Stdout, resp.Body)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: read: %v", err)
-		}
-		fmt.Printf("<------------Body Section-------->\n")
-		fmt.Printf("%v\n", b)
-		fmt.Printf("<------------Header Section-------->\n")
-		fmt.Printf("%v\n", resp.Header)
+func main() {
+	flag.Parse()
+	fmt.Printf(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
 	}
+
 }
